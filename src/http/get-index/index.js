bcd7b3e6bbf7e1ed9d582edb70fdcb522b640ac0
kolejoseph <50643819+kolejoseph@users.noreply.github.com>
2020-01-26 19:43:29 -0800

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -39,6 +39,8 @@ exports.handler = async function Index () {
       'content-type': 'text/html; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
-    body
+    <body>
+      <a href="https://coolmathgames.com"
+    <body>
   }
 }
